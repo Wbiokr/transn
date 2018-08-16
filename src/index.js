3ec3cd01575d3bf931df02a9f00e70cb4ad71d17
Wbiokr <Wbiokr@hotmail.com>
2018-08-16 14:58:21 +0800

    传神vue组件共享npm发包

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import toast from './components/toast/index.js'
+import page from './components/page/index.vue'
+
+const transn={
+  toast,
+  page,
+}
+
+export default transn;
+
+export {toast,page};
